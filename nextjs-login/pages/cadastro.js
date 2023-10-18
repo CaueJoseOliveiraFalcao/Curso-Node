7c65336d3edc6efaa5e484f356aed4c1607d5da8
Cauê Josê Oliveira Falcão <cauejose007@gmail.com>
2023-10-18 16:35:59 -0400

    df

--- nextjs-login/pages/cadastro.js
+++ nextjs-login/pages/cadastro.js
@@ -1,7 +1,18 @@
+import LoginCard from "../src/components/loginCard/loginCard";
+import styles from "../styles/login.module.css"
+import Input from "../src/components/inputs/input";
+import Button from "../src/components/button/button";
 export default function Cadastro(){
     return(
-        <div>
-            Cadastro
+        <div className={styles.background}>
+             <LoginCard  title="Crie sua Conta">
+                <form className={styles.form}>
+                    <Input type='text' placeholder="seu nome"/>
+                    <Input type='text' placeholder="seu email"/>
+                    <Input type='password' placeholder="sua senha"/>
+                    <Button>Criar</Button>
+                </form>
+            </LoginCard>
         </div>
     )
 }
--- nextjs-login/pages/login.js
+++ nextjs-login/pages/login.js
@@ -1,7 +1,18 @@
+import LoginCard from "../src/components/loginCard/loginCard"
+import styles from "../styles/login.module.css"
+import Input from "../src/components/inputs/input"
+import Button from "../src/components/button/button"
+Button
 export default function LoginPage(){
     return(
-        <div>
-            LoginPage
+        <div className={styles.background}>
+            <LoginCard  title="Entre em sua Conta">
+                <form className={styles.form}>
+                    <Input type='text' placeholder="seu email"/>
+                    <Input type='password' placeholder="sua senha"/>
+                    <Button>Entrar</Button>
+                </form>
+            </LoginCard>
         </div>
     )
 }
